a3ad9c548f6483f8a5035062ea1797d96dec789d
OTEC <otec1klas@gmail.com>
2016-06-03 22:29:11 +0300

    install Avada & WPML

--- wp-config.php
+++ wp-config.php
@@ -83,7 +83,7 @@ $table_prefix  = 'os_';
  * @link https://codex.wordpress.org/Debugging_in_WordPress
  */
 define('WP_DEBUG', false);
-
+define('WP_MEMORY_LIMIT', '256M');
 /* That's all, stop editing! Happy blogging. */
 
 /** Absolute path to the WordPress directory. */
